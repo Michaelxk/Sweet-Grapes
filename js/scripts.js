2be45e817bf7cbd6c88ac17cb9328cd59fe9b538
Michael Linares <macbookair@Mike-Mac.local>
2020-03-20 10:03:19 -0400

    Adding files to my first commit

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+let menu = document.querySelector("#menu");
+let toggleIicon = document.querySelector("#toggle-icon");
+
+toggleIicon.addEventListener("click", function() {
+  menu.classList.toggle("menu-on");
+});
